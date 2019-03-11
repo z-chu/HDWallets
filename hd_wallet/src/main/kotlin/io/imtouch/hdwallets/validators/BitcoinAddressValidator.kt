f4175eb05197062804fb88e01e812bf75a7aa8ec
zchu <zchu8073@gmail.com>
2019-03-11 11:38:08 +0800

    Update BitcoinAddressValidator

--- hd_wallet/src/main/kotlin/io/imtouch/hdwallets/validators/BitcoinAddressValidator.kt
+++ hd_wallet/src/main/kotlin/io/imtouch/hdwallets/validators/BitcoinAddressValidator.kt
@@ -3,6 +3,7 @@ package io.imtouch.hdwallets.validators
 import org.bitcoinj.core.Address
 import org.bitcoinj.core.AddressFormatException
 import org.bitcoinj.core.NetworkParameters
+import org.bitcoinj.params.*
 
 /**
  * Created by xyz on 2018/3/12.
@@ -12,14 +13,43 @@ object BitcoinAddressValidator {
 
 
     @JvmStatic
-    fun isValidAddress(networkParameters: NetworkParameters, addressBase58: String): Boolean {
+    fun isValidAddress(addressBase58: String, networkParameters: NetworkParameters): Boolean {
         return try {
             Address.fromBase58(networkParameters, addressBase58)
             true
-        } catch (e: AddressFormatException) {
-            e.printStackTrace()
+        } catch (ignore: AddressFormatException) {
             false
         }
+
+    }
+
+    /**
+     * 验证地址是否合法，任意一个 NetworkParameters 通过就算合法
+     */
+    @JvmStatic
+    fun isValidAddress(addressBase58: String): Boolean {
+        return getNetParamsByAddress(addressBase58) != null
+    }
+
+
+    /**
+     * 通过 Address 找到所对应的 NetworkParameters
+     */
+    @JvmStatic
+    fun getNetParamsByAddress(addressBase58: String): NetworkParameters? {
+        val netParamsList = arrayListOf(
+            MainNetParams.get(),
+            TestNet3Params.get(),
+            UnitTestParams.get(),
+            RegTestParams.get(),
+            TestNet2Params.get()
+        )
+        for (netParams in netParamsList) {
+            if (isValidAddress(addressBase58, netParams)) {
+                return netParams
+            }
+        }
+        return null
     }
 
 
--- hd_wallet/src/test/kotlin/io/imtouch/hdwallets/BitcoinTransactionTest.kt
+++ hd_wallet/src/test/kotlin/io/imtouch/hdwallets/BitcoinTransactionTest.kt
@@ -2,8 +2,7 @@ package io.imtouch.hdwallets
 
 import io.imtouch.hdwallets.address.BitcoinAddressCreator
 import io.imtouch.hdwallets.transaction.BitcoinTransaction
-import org.bitcoinj.core.Address
-import org.bitcoinj.core.AddressFormatException
+import io.imtouch.hdwallets.validators.BitcoinAddressValidator
 import org.bitcoinj.params.MainNetParams
 import org.bitcoinj.params.TestNet3Params
 import org.junit.Assert
@@ -27,8 +26,6 @@ class BitcoinTransactionTest {
         )
 
 
-
-
         val signedTransaction = BitcoinTransaction(TestNet3Params.get()).signTransaction(
             generateKey.keyPair,
             "01000000017335aed204417492dadd3fdecabcd2f80c35cc374a47f4162a06f93176308aaf010000006a47304402204d666ca4c7234536b9aadfd0eb0ec3b2dca61acc2f57a6b0c9672661b760990602202ffe635d91effa77247e0d46212c84981be2bd654778929e7bd5736e3e4da60701210254dec37f0858dd993798f8b31ba912eb3cee803ac4209596cc79c804a2f3c201ffffffff0210270000000000001976a91415c5e0965754cd540c719aac3e52d36b2d9a815288ac28cfb500000000001976a91415c5e0965754cd540c719aac3e52d36b2d9a815288ac00000000"
@@ -42,21 +39,27 @@ class BitcoinTransactionTest {
         )
     }
 
-    @Test(expected = AddressFormatException::class)
+    @Test
     fun bitcoinIllegalAddress() {
-        Address.fromBase58(MainNetParams.get(),"cUQ2Xzvmyn8JpvoBpQ2jEBWBgGjAS2A4fQSigWYD3B1KHKo6box7")
-        Address.fromBase58(MainNetParams.get(),"03add289da9f31e1d468c99e2d83ebe1fd6f403bad9928177011a9ffbb6d70b73a")
-        Address.fromBase58(MainNetParams.get(),"0xEDa145386Ae0e6990318c94b4529CBe8F0B5b21C")
-        Address.fromBase58(MainNetParams.get(),"n1fmDsaTWcWdDZvr5vgia4JHascTen6Xy4")
+        Assert.assertEquals(
+            BitcoinAddressValidator.isValidAddress("cUQ2Xzvmyn8JpvoBpQ2jEBWBgGjAS2A4fQSigWYD3B1KHKo6box7"),
+            false
+        )
+        Assert.assertEquals(
+            BitcoinAddressValidator.isValidAddress("03add289da9f31e1d468c99e2d83ebe1fd6f403bad9928177011a9ffbb6d70b73a"),
+            false
+        )
+        Assert.assertEquals(BitcoinAddressValidator.isValidAddress("0xEDa145386Ae0e6990318c94b4529CBe8F0B5b21C"), false)
+        Assert.assertEquals(BitcoinAddressValidator.isValidAddress("n1fmDsaTWcWdDZvr5vgia4JHascTen6Xy4",MainNetParams.get()), false)
     }
 
     @Test
     fun bitcoinCorrectAddress() {
-        Address.fromBase58(MainNetParams.get(),"1BcUmJd8XxFqhrji5ZBF2Dbxkj3pDr3dqt")
-        Address.fromBase58(MainNetParams.get(),"15Ag5i8SjRuJhQnqUpJtkeCk7DogpKBG1A")
-        Address.fromBase58(MainNetParams.get(),"16ykMnFhBJWpxsVju8LD91CuCaK3faFWzy")
-        Address.fromBase58(TestNet3Params.get(),"n1fmDsaTWcWdDZvr5vgia4JHascTen6Xy4")
-        Address.fromBase58(TestNet3Params.get(),"mitF3CY8vY8uNdb9pKrDyLt74S52q8Nehk")
-        Address.fromBase58(TestNet3Params.get(),"mvBXH1Nh1xehRRXaWvym6LrYT9YVkXLB3T")
+        BitcoinAddressValidator.isValidAddress("1BcUmJd8XxFqhrji5ZBF2Dbxkj3pDr3dqt", MainNetParams.get())
+        BitcoinAddressValidator.isValidAddress("15Ag5i8SjRuJhQnqUpJtkeCk7DogpKBG1A", MainNetParams.get())
+        BitcoinAddressValidator.isValidAddress("16ykMnFhBJWpxsVju8LD91CuCaK3faFWzy", MainNetParams.get())
+        BitcoinAddressValidator.isValidAddress("n1fmDsaTWcWdDZvr5vgia4JHascTen6Xy4", TestNet3Params.get())
+        BitcoinAddressValidator.isValidAddress("mitF3CY8vY8uNdb9pKrDyLt74S52q8Nehk", TestNet3Params.get())
+        BitcoinAddressValidator.isValidAddress("mvBXH1Nh1xehRRXaWvym6LrYT9YVkXLB3T", TestNet3Params.get())
     }
 }
